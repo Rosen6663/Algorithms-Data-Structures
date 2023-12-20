2c00db65218fa2e0d9cd5f69e98f057cdb9ec196
roesn <rosen@qq.com>
2023-12-20 11:39:25 +0800

    123

---
+++ 二叉树/线索二叉树.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+//创建树 
+typedef struct BiTNode{
+	char data;
+	struct BiTNode *lchild,*rchild;
+	int ltag,rtag;
+}BiTNode,*BiTree;
+//创建根节点 
+void InitBiTree(BiTree &root)
+{
+	
+	root->data='A';
+	root->ltag=root->rtag=0;
+	root->lchild=NULL;
+	root->rchild=NULL;
+}
+
+int main()
+{
+	//创建二叉树 
+	BiTree root=(BiTree)malloc(sizeof(BiTNode));
+
+	InitBiTree(root);
+
+	BiTNode*b1=(BiTNode*)malloc(sizeof(BiTNode));
+	b1->data='B';
+	b1->rchild=NULL;
+	b1->lchild=NULL;
+	b1->ltag=root->rtag=0;
+	root->lchild=b1;
+
+	
+	BiTNode*b2=(BiTNode*)malloc(sizeof(BiTNode));
+	b2->data='C';
+	b2->rchild=NULL;
+	b2->lchild=NULL;
+	b2->ltag=root->rtag=0;
+	root->rchild=b2;
+
+	BiTNode*b3=(BiTNode*)malloc(sizeof(BiTNode));
+	b3->data='D';
+	b3->rchild=NULL;
+	b3->lchild=NULL;
+	b3->ltag=root->rtag=0;
+	root->lchild->rchild=b3;
+	
+	BiTNode*b4=(BiTNode*)malloc(sizeof(BiTNode));
+	b4->data='E';
+	b4->rchild=NULL;
+	b4->lchild=NULL;
+	b4->ltag=root->rtag=0;
+	root->rchild->lchild=b4;
+	
+	printf("%c\n",root->data);
+	printf("%c\n",root->lchild->data);
+	printf("%c\n",root->rchild->data);
+	printf("%c\n",root->lchild->rchild->data);
+	printf("%c\n",root->rchild->lchild->data);
+	
+	//二叉树的线索化 
+	
+} 
